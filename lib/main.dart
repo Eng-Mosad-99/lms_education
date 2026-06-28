23ad1ea34acec8cc09d97749171fe67cca7d35b1
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-06-28 09:34:12 -0400

    refactor: initialize Firebase in main function and clean up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lms/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const LmsEducation());
 }
 
 class LmsEducation extends StatelessWidget {
   const LmsEducation({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
@@ -20,11 +22,8 @@ class LmsEducation extends StatelessWidget {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("LMS"),
         ),
-        body: const Center(
-          child: Text("Welcome to LMS"),
-        ),
+        body: const Center(child: Text("Welcome to LMS")),
       ),
     );
   }
 }
-
