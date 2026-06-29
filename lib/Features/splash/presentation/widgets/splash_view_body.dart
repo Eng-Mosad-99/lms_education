908cb41e6120fdbe601ccafcd9b457fe04645120
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-06-29 09:31:47 -0400

    feat: implement onboarding navigation and create OnboardingView component

--- lib/Features/splash/presentation/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/widgets/splash_view_body.dart
@@ -1,11 +1,24 @@
 
 import 'package:flutter/material.dart';
 
+import '../../../../core/constants/constants.dart';
+import '../../../../core/routes/routes.dart';
+import '../../../../core/services/shared_prefs.dart';
 import '../../../../core/utils/app_assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    executeNavigation();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,5 +26,17 @@ class SplashViewBody extends StatelessWidget {
       alignment: Alignment.center,
      child: Image.asset(Assets.imagesAppLogo),
     );
+  }
+  
+  void executeNavigation() {
+     var isOnBoardingViewSeen =
+        PrefsSingleton.getBool(KIsOnBoardingViewSeen) ?? false;
+    Future.delayed(const Duration(seconds: 2), () {
+      if (isOnBoardingViewSeen) {
+        Navigator.pushReplacementNamed(context, Routes.loginRoute);
+      } else {
+        Navigator.pushReplacementNamed(context, Routes.onboardingRoute);
+      }
+    });
   }
 }
--- lib/core/routes/generate_route.dart
+++ lib/core/routes/generate_route.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:lms/Features/splash/presentation/views/splash_view.dart';
 import 'package:lms/core/routes/routes.dart';
 
+import '../../Features/boarding/presentation/views/onboarding_view.dart';
+
 
 Route<dynamic>? generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case Routes.splashRoute:
       return MaterialPageRoute(builder: (_) => SplashView());
-    // case Routes.onboardingRoute:
-    //   return MaterialPageRoute(builder: (_) => const OnboardingView());
+    case Routes.onboardingRoute:
+      return MaterialPageRoute(builder: (_) => const OnboardingView());
     
     default:
       return null;
