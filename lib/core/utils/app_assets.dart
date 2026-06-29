2014cc7ee97400e50d7db78f546061ed5f0c3c3e
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-06-28 22:19:43 -0400

    Add asset paths for images and icons in app_assets.dart

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.png
+  static const String imagesAppLogo = "assets/images/app_logo.png";
+
+  /// Assets for imagesArrowLeftIcon
+  /// assets/images/arrow_left_icon.svg
+  static const String imagesArrowLeftIcon = "assets/images/arrow_left_icon.svg";
+
+  /// Assets for imagesBookmarkIcon
+  /// assets/images/bookmark_icon.svg
+  static const String imagesBookmarkIcon = "assets/images/bookmark_icon.svg";
+
+  /// Assets for imagesFacebookIcon
+  /// assets/images/facebook_icon.svg
+  static const String imagesFacebookIcon = "assets/images/facebook_icon.svg";
+
+  /// Assets for imagesGoogleIcon
+  /// assets/images/google_icon.svg
+  static const String imagesGoogleIcon = "assets/images/google_icon.svg";
+
+  /// Assets for imagesNotificationIcon
+  /// assets/images/notification_icon.svg
+  static const String imagesNotificationIcon = "assets/images/notification_icon.svg";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesOnboarding4
+  /// assets/images/onboarding4.png
+  static const String imagesOnboarding4 = "assets/images/onboarding4.png";
+
+  /// Assets for imagesOnboarding5
+  /// assets/images/onboarding5.png
+  static const String imagesOnboarding5 = "assets/images/onboarding5.png";
+
+  /// Assets for imagesSettingIcon
+  /// assets/images/setting_icon.svg
+  static const String imagesSettingIcon = "assets/images/setting_icon.svg";
+
+  /// Assets for imagesSuccessfulCheck
+  /// assets/images/successful_check.png
+  static const String imagesSuccessfulCheck = "assets/images/successful_check.png";
+
+  /// Assets for imagesTransactionCompleted
+  /// assets/images/transaction_completed.png
+  static const String imagesTransactionCompleted = "assets/images/transaction_completed.png";
+}
+
