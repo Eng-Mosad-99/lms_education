27d624457c09205e40e94b3ff914f5bd4eba8144
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-06-30 08:09:08 -0400

    feat: update onboarding content with descriptive titles and bodies

--- lib/Features/boarding/data/models/boarding_model.dart
+++ lib/Features/boarding/data/models/boarding_model.dart
@@ -15,27 +15,27 @@ class BoardingModel {
 final List<BoardingModel> boardingList = [
   BoardingModel(
     image: Assets.imagesOnboarding1,
-    title: 'Title 1',
-    body: 'Body 1',
+    title: 'Welcome to Cybex IT Group\nwhere learning meets innovation!',
+    body: 'Empowering your journey through cutting-edge IT education and expertise',
   ),
   BoardingModel(
     image: Assets.imagesOnboarding2,
-    title: 'Title 2',
-    body: 'Body 2',
+    title: 'Begin your learning journey and unlock a world of knowledge',
+    body: 'Explore our comprehensive courses designed to transform your skills and career',
   ),
   BoardingModel(
     image: Assets.imagesOnboarding3,
-    title: 'Title 3',
-    body: 'Body 3',
+    title: 'Dive into a seamless learning experience with Cybex IT Group',
+    body: 'Experience interactive learning with expert-led courses and progress tracking',
   ),
   BoardingModel(
     image: Assets.imagesOnboarding4,
-    title: 'Title 3',
-    body: 'Body 3',
+    title: 'Join a community of learners and embark on a learning adventure',
+    body: 'Connect with like-minded individuals Join us to learn, grow, and thrive together!',
   ),
   BoardingModel(
     image: Assets.imagesOnboarding5,
-    title: 'Title 3',
-    body: 'Body 3',
+    title: 'Join Cybex IT Group to Kick Start Your Lesson',
+    body: 'Join and Learn from our Top Instructors!',
   ),
 ];
